8d79f098c0232ca9fcb84c6e1b92a5e6ef60b76f
LanserJ <50208676+LanserJ@users.noreply.github.com>
2019-05-20 10:41:56 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 import socket
-import select
 import errno
 
-HEADER_LENGHT = 10
+HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 9999
 
@@ -13,32 +12,33 @@ client_socket.connect((IP,PORT))
 client_socket.setblocking(False)
 
 username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEADER_LENGHT}}".encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 client_socket.send(username_header + username)
 
 while True:
     message = input(f"{my_username} > ")
     
     if message:
-        message = message.encode('uft-8')
-        message_header = f"{len(message) :< {HEADER_LENGHT}}".encode('utf-8')
+        message = message.encode('utf-8')
+        message_header = f"{len(message) :< {HEADER_LENGTH}}".encode('utf-8')
         client_socket.send(message_header + message)
         
     try:
         while True:
-            username_header = client_socket.recv(HEADER_LENGHT)
+            username_header = client_socket.recv(HEADER_LENGTH)
             if not len(username_header):
                 print("Verbindung zum Server wurde getrennt")
                 sys.exit()
-            username_lenght = int(username_lenght.decode('utf-8').strip())
-            username = client_socket.recv(username_lenght).decode('utf-8')
+            username_length = int(username_header.decode('utf-8').strip())
+            username = client_socket.recv(username_length).decode('utf-8')
             
-            message_header = client_socket.recv(HEADER_LENGHT)
+            message_header = client_socket.recv(HEADER_LENGTH)
             message_lenght = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_lenght).decode('utf-8')
             
             print(f"{username} > {message}")
             
-    except IOERROR as e:
+    except IOError as e:
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
             print('Nachricht konnte nicht gelesen werden', str(e))
             sys.exit()
